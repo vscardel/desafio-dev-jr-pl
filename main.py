9a7a7ce84589216b4b3c9c8d89224494f5a1a6b3
vscardel <vscardel@gmail.com>
2021-08-04 11:36:26 -0300

    implemented save graph functionality with unit tests

--- main.py
+++ main.py
@@ -39,5 +39,22 @@ def save_graph():
 	#if not json returns bad request code
 	return Response(json_data, status=400)
 
+#retrieves a graph with id "graph_id from the database"
+@app.route("/graph/<int:graph_id>")
+def retrieve_graph(graph_id):
+	conn = mysql.connect()
+	cursor = conn.cursor()
+	retrieve_query = "SELECT * FROM Graphs WHERE id = %s"
+	try:
+		cursor.execute(retrieve_query,str(graph_id))
+	except:
+		return Response("",status=500)
+	graph = cursor.fetchone()
+	if graph:
+		return jsonify(graph),200
+	else:
+		return Response("",status=404)
+	conn.close()
+
 if __name__ == '__main__':
 	app.run(host="0.0.0.0",debug=True,port=8080)
--- test.py
+++ test.py
@@ -8,7 +8,7 @@ class FlaskTestCase(unittest.TestCase):
 
 	#test if the http response for sucessfully storing a graph
 	#is 201
-	def test_store_sucessfull_response_code(self):
+	def test_save_graph_sucessfull_response_code(self):
 		test_graph = graphs['test_graph_1']
 		url = "http://localhost:8080/graph"
 		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
@@ -18,7 +18,7 @@ class FlaskTestCase(unittest.TestCase):
 		self.assertEqual(response.status_code,201)
 
 	#check if the store payload returns the id
-	def test_store_sucessful_payload(self):
+	def test_save_graph_sucessfull_payload(self):
 		test_graph = graphs['test_graph_1']
 		url = "http://localhost:8080/graph"
 		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
@@ -28,7 +28,7 @@ class FlaskTestCase(unittest.TestCase):
 		self.assertTrue(response.json()['id'])
 
 	#tests if storing returns the code 400 for an empty graph
-	def test_store_bad_request(self):
+	def test_save_graph_bad_request(self):
 		test_graph = graphs['None']
 		url = "http://localhost:8080/graph"
 		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
@@ -36,6 +36,28 @@ class FlaskTestCase(unittest.TestCase):
 			data=json.dumps(test_graph), 
 			headers=headers)
 		self.assertEqual(response.status_code,400)
+
+	#test if the response code for an existing graph is 200
+	def test_retrieve_graph_sucessfull_response_code(self):
+		url = "http://localhost:8080/graph/14"
+		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+		response = requests.get(url,headers=headers)
+		self.assertEqual(response.status_code,200)
+
+	#tests if the json graph is returned by retrieve_graph
+	def test_retrieve_graph_returns_json_response(self):
+		url = "http://localhost:8080/graph/14"
+		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+		response = requests.get(url,headers=headers)
+		self.assertTrue(response.json())
+
+	#test if receive_graph returns 404 for non existing graph on db
+	def test_retrive_graph_graph_dont_exists(self):
+		#id -1 cannot exist on db
+		url = "http://localhost:8080/graph/-1"
+		headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+		response = requests.get(url,headers=headers)
+		self.assertEqual(response.status_code,404)
 
 
 if __name__ == '__main__':
